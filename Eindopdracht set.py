6fba7670ae6275c81bbf4ac679b305d417d71324
LoLo0310 <l.c.bolle@students.uu.nl>
2022-06-14 12:23:35 +0200

    Update Eindopdracht set.py

--- Eindopdracht set.py
+++ Eindopdracht set.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun  6 13:21:08 2022
-
-@author: sterrecorver
-"""
-
 import pygame
 
+class Kaarten:
+    
+    def __init__(self,kleur,vorm,vulling,aantal):
+        self.kleur=kleur
+        self.vorm=vorm
+        self.vulling=vulling
+        self.aantal=aantal
 
 
 
