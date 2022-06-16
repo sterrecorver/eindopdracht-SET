8aee9f2970c92ddb088927fa5f3bc12cc82274da
sterrecorver <sterrecorver@gmail.com>
2022-06-16 15:53:24 +0200

    Update Eindopdracht set.py

--- Eindopdracht set.py
+++ Eindopdracht set.py
@@ -1,28 +1,44 @@
 import pygame
+import random
 
 class Kaarten:
-    
     def __init__(self,kleur,vorm,vulling,aantal):
         self.kleur=kleur
         self.vorm=vorm
         self.vulling=vulling
         self.aantal=aantal
-            
-        kleur=['green','purple','red']
-        vorm=['diamond','oval','squiggle']
-        vulling=['empty', 'filled', 'shaded']
-        aantal=['1','2','3']
-    
-    
+        
+
+kleur=['green','purple','red']
+vorm=['diamond','oval','squiggle']
+vulling=['empty', 'filled', 'shaded']
+aantal=['1','2','3']
+
+
+deck = []
 for kleur in ['green','purple','red']:
     for vorm in ['diamond','oval','squiggle']:
         for vulling in ['empty', 'filled', 'shaded']:
             for aantal in ['1','2','3']:
+                kaarten_toevoegen = Kaarten(kleur, vorm, vulling, aantal)
+                deck.append(kaarten_toevoegen)
                 pygame.image.load('/Users/sterrecorver/Documents/Kaarten/'+kleur+vorm+vulling+aantal+'.gif')
         
         
-        
-
+def gelijk_of_ongelijk(k1,k2,k3):
+    if k1 == k2 and k2 == k3:
+        return True
+    elif k1 != k2 and k2 != k3 and k3 != k1:
+        return True
+    else:
+        return False
+    
+def set_of_niet(kaart_1,kaart_2,kaart_3):
+    controle_kleur = gelijk_of_ongelijk(kaart_1.kleur, kaart_2.kleur, kaart_3.kleur)
+    controle_vorm = gelijk_of_ongelijk(kaart_1.vorm, kaart_2.vorm, kaart_3.vorm)
+    controle_vulling = gelijk_of_ongelijk(kaart_1.vulling, kaart_2.vulling, kaart_3.vulling)
+    controle_aantal = gelijk_of_ongelijk(kaart_1.aantal, kaart_2.aantal, kaart_3.aantal)
+    return controle_kleur and controle_vorm and controle_vulling and controle_aantal
 
 
 
